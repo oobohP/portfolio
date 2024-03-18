aa3222c33af96c267371480dd7ad1a1f45775d95
Steven Ly <stevenly1616@gmail.com>
2024-03-18 15:31:04 -0700

    update validation to send emails

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,22 +2,45 @@ import { revalidateTag } from "next/cache";
 import { parseBody } from "next-sanity/webhook";
 import { type NextRequest, NextResponse } from "next/server";
 
+interface WebhookBody {
+  _type: string;
+  slug: {
+      _type: 'slug';
+      current: string;
+  };
+  title: string;
+  subtitle: string;
+}
+
+
 export async function POST(req: NextRequest) {
   try {
-    const { body, isValidSignature } = await parseBody<{
-      _type: string;
-      slug?: string | undefined;
-    }>(req, process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET);
+    const { body, isValidSignature } = await parseBody<WebhookBody>(req, process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET);
 
     if (!isValidSignature) {
       return new Response("Invalid Signature", { status: 401 });
     }
 
-    if (!body?._type) {
+    if (!body?._type || !body.slug?.current) {
       return new Response("Bad Request", { status: 400 });
     }
 
     revalidateTag(body._type);
+
+    const blogData = {
+      blogTitle: body.title,
+      blogExcerpt: body.subtitle,
+      slug: body.slug.current
+    };
+
+    await fetch('https://development-portfolio-git-feature-add-email-on-sanity-phobo.vercel.app/api/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(blogData)
+    });
+
     console.log(body)
     console.log('Revalidated', body._type, 'at', Date.now());
     return NextResponse.json({
--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -6,11 +6,13 @@ export async function POST(request: Request) {
   const response = await request.json();
   const email = response.email
 
+  if (!process.env.RESEND_AUDIENCE_ID) { throw new Error('No audience ID found') }
+
   try {
     resend.contacts.create({
       email: email,
       unsubscribed: false,
-      audienceId: process.env.RESEND_AUDIENCE_ID || ''
+      audienceId: process.env.RESEND_AUDIENCE_ID
     });
   } catch (error) {
     console.error('error', error);
