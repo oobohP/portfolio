22a92eb53794069d6e2c10dde4ad1227f73d1c0c
Steven Ly <stevenly1616@gmail.com>
2024-03-18 14:32:25 -0700

    revert changes

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,22 +2,12 @@ import { revalidateTag } from "next/cache";
 import { parseBody } from "next-sanity/webhook";
 import { type NextRequest, NextResponse } from "next/server";
 
-// Define the expected structure of the body and resultBody
-interface WebhookBody {
-    _type: string;
-    slug?: string;
-    resultBody?: {
-        title: string;
-        subtitle: string;
-        slug: {
-            current: string;
-        };
-    };
-}
-
 export async function POST(req: NextRequest) {
   try {
-    const { body, isValidSignature } = await parseBody<WebhookBody>(req, process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET);
+    const { body, isValidSignature } = await parseBody<{
+      _type: string;
+      slug?: string | undefined;
+    }>(req, process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET);
 
     if (!isValidSignature) {
       return new Response("Invalid Signature", { status: 401 });
@@ -27,29 +17,8 @@ export async function POST(req: NextRequest) {
       return new Response("Bad Request", { status: 400 });
     }
 
-    if (!body.resultBody) {
-      console.error('resultBody is missing in the webhook payload');
-      return new Response("Bad Request", { status: 400 });
-    }
-
-    const { title, subtitle, slug } = body.resultBody;
-
     revalidateTag(body._type);
-
-    const blogData = {
-      blogTitle: title,
-      blogExcerpt: subtitle,
-      slug: slug?.current
-    };
-
-    await fetch('https://staging.stevenly.dev/api/send', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(blogData)
-    });
-
+    console.log(body)
     console.log('Revalidated', body._type, 'at', Date.now());
     return NextResponse.json({
       status: 200,
