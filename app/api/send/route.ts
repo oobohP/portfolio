a565ee632c26d800de78febf31f4bca5200aff07
Steven Ly <stevenly1616@gmail.com>
2024-03-18 00:12:25 -0700

    add logic for sending batch emails

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -6,17 +6,29 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['testing@resend.dev'],
-      subject: 'Test Email',
-      react: EmailTemplate({ firstName: 'Steven', blogTitle: 'Hello World', blogExcerpt: 'This is a test email', blogUrl: 'https://resend.dev'}),
+    const response = await resend.contacts.list({ audienceId: process.env.RESEND_AUDIENCE_ID || '' });
+
+    if (!response.data.data) {
+      console.error('No Contacts found in audiences', response)
+      return Response.json({ error: 'No contacts found' });
+    }
+
+    const contacts = response.data?.data;
+
+    // Map each contact to an email template object
+    const emailsToSend = contacts.map(contact => {
+      return {
+        from: 'Steven <sly@stevenly.dev>',
+        to: contact.email,
+        subject: 'Test Email Batch 2',
+        react: EmailTemplate({ firstName: contact.email, blogTitle: 'Hello World', blogExcerpt: 'This is a test email', blogUrl: 'https://resend.dev' })
+      };
     });
 
-    console.log('email sent')
-    return Response.json(data);
+    const data = await resend.batch.send(emailsToSend);
+    return new Response('Emails sent', { status: 200 });
   } catch (error) {
-    console.error('error', error);
-    return Response.json({ error });
+    console.error('Error sending emails', error);
+    return new Response(JSON.stringify({ error: 'Error processing request' }), { status: 500 });
   }
 }
